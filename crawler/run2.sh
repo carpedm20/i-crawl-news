9e221b56750fa4be4d3257f8b7b8b44c8a6c29a0
carpedm20 <carpedm20@gmail.com>
2015-01-04 13:50:59 +0900

    update

--- crawler/run2.sh
+++ crawler/run2.sh
@@ -8,7 +8,7 @@ do
     for j in $(seq 1 12);
     do
       echo "$name $i $j"
-      scrapy crawl deepnews -a month=$j -a year=$i -a query=$name -o "$name-$i-$j".json
+      scrapy crawl deepnews -a month=$j -a year=$i -a query=$name -o "$name-$i-$j-deep".json
     done
   done
 done < $1
