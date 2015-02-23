3bc14d813c67bd1ee2f71db5b25df92a8f04d171
carpedm20 <carpedm20@gmail.com>
2015-02-23 13:49:05 +0900

    update movie parser run5.sh

---
+++ crawler/run5.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+
+name="interstellar movie" #2014,2015
+#name="the dark knight" #2008,2009
+#name="inception movie" #2010,2011
+
+#name="The Avengers movie" #2012,2013
+#name="Frozen movie" #2012,2013
+
+#name="the departed movie" #2006,2007
+#name="the intouchables movie" #2012,2013
+
+declare -a foo1=("the dark knight" "inception movie" "the avengers movie" "frozen movie" "skyfall movie")
+declare -a foo2=("2008" "2010" "2012" "2013" "2012")
+declare -a foo3=("2009" "2011" "2012" "2014" "2013")
+
+for i in "${!foo1[@]}";
+do
+  name=${foo1[$i]}
+  syear=${foo2[$i]}
+  eyear=${foo3[$i]}
+    
+  for i in $(seq $syear $eyear);
+  do
+    for j in $(seq 1 12);
+    do
+      file="./backup/$name-$i-$j.json" 
+      if [ ! -f "$file" ]; then
+        #echo "File $file not found!"
+        echo "$name $i $j"
+        scrapy crawl news -a month="$j" -a year=$i -a query="$name" -o "$file"
+      else
+        echo "File $file exist!"
+        minimumsize=100
+        actualsize=$(wc -c "$file" | cut -f 1 -d ' ')
+        if [ $actualsize -ge $minimumsize ]; then
+          echo " ==> skip"
+        else
+          echo " ==> need crawl"
+          rm "$file"
+          echo scrapy crawl news -a month=$j -a year=$i -a query="$name" -o "$file"
+          scrapy crawl news -a month=$j -a year=$i -a query="$name" -o "$file"
+          echo size is under $minimumsize bytes
+        fi
+      fi
+    done
+  done
+done
--- crawler/test.sh
+++ crawler/test.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
-for i in {1..5}; do echo $i; done
+declare -a foo1=("element1" "element2" "element3")
+declare -a foo2=("eaa a" "bbb b" "element3")
+
+for i in "${!foo1[@]}"; do 
+    printf "%s\t%s\t%s\n" "$i" "${foo1[$i]}" "${foo2[$i]}"
+done
